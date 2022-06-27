476bbaaa3258b6542d37574e98761fa84ca004b7
alex100397 <alexjunia@gmail.com>
2022-06-27 23:25:38 +0530

    rel=noopener

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,11 +9,11 @@ const Footer = () => {
             </span>
             <hr style={{width:"90%"}}/>
             <div className='iconContainer'>
-               <a href="https://www.facebook.com/alex.junia/" target="_blank"> 
+               <a href="https://www.facebook.com/alex.junia/" rel="noopener"> 
                 <i class="fa-brands fa-facebook"/></a>
-                <a href="https://www.instagram.com/alexjunia/" target="_blank">
+                <a href="https://www.instagram.com/alexjunia/" rel="noopener">
                 <i class="fa-brands fa-instagram"/></a>
-                <a href="https://www.linkedin.com/in/alex-junia/" target="_blank">
+                <a href="https://www.linkedin.com/in/alex-junia/" rel="noopener">
                 <i class="fa-brands fa-linkedin"/></a>
             </div>
         </div>
